85976aeb3c9f4ca9ae574433767e90c2abcbf85b
Victor Pereira <victtor.vpb@gmail.com>
2017-06-07 22:15:09 -0300

    appling babel

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # study-react-basic
 ## Repository of study react
+ **Complile code js com** _babel --presets react ./files/js --watch --out-dir build_
---
+++ build/main.js
@@ -0,0 +1,49 @@
+var Nav = React.createClass({
+    displayName: "Nav",
+
+    render: function () {
+        return React.createElement(
+            "nav",
+            { className: "navbar navbar-default" },
+            React.createElement(
+                "div",
+                { className: "container" },
+                React.createElement(
+                    "div",
+                    { className: "navbar-header" },
+                    React.createElement(
+                        "a",
+                        { href: this.props.linkUrl, className: "navbar-brand" },
+                        this.props.title
+                    )
+                )
+            )
+        );
+    }
+});
+
+var Title = React.createClass({
+    displayName: "Title",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: "container" },
+            React.createElement(
+                "div",
+                { className: "row" },
+                React.createElement(
+                    "h1",
+                    null,
+                    " ",
+                    this.props.title,
+                    " "
+                )
+            )
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(Nav, { title: "Learn React!", linkUrl: "index.html" }), document.getElementById('nav'));
+
+ReactDOM.render(React.createElement(Title, { title: "Learning React" }), document.getElementById('title'));
