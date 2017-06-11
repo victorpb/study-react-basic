c94cc469b8e0538aca65602a4b777f4559551fed
Victor Pereira <victtor.vpb@gmail.com>
2017-06-11 13:26:29 -0300

    state react

--- build/main.js
+++ build/main.js
@@ -44,6 +44,36 @@ var Title = React.createClass({
     }
 });
 
+var Button = React.createClass({
+    displayName: "Button",
+
+
+    getInitialState: function () {
+        return {
+            click: false
+        };
+    },
+
+    toggleClick: function () {
+        this.setState({
+            click: !this.state.click
+        });
+    },
+
+    render: function () {
+        var btnClass = this.state.click ? 'btn btn-success' : 'btn btn-default';
+        var text = this.state.click ? this.props.textActive : this.props.text;
+        return React.createElement(
+            "button",
+            { onClick: this.toggleClick, className: btnClass },
+            " ",
+            text
+        );
+    }
+});
+
 ReactDOM.render(React.createElement(Nav, { title: "Learn React!", linkUrl: "index.html" }), document.getElementById('nav'));
 
 ReactDOM.render(React.createElement(Title, { title: "Learning React" }), document.getElementById('title'));
+
+ReactDOM.render(React.createElement(Button, { text: "My component button", textActive: "Button Active" }), document.getElementById('button'));
--- files/js/main.js
+++ files/js/main.js
@@ -26,6 +26,30 @@ var Title = React.createClass({
     }
 });
 
+var Button = React.createClass({
+
+    getInitialState: function() {
+        return {
+            click: false,
+        };
+    },
+
+    toggleClick: function() {
+        this.setState({
+            click: !this.state.click,
+        });
+        
+    },
+
+    render: function(){
+        var btnClass = this.state.click ? 'btn btn-success': 'btn btn-default';
+        var text = this.state.click ? this.props.textActive: this.props.text;
+       return(
+        <button onClick={this.toggleClick} className={ btnClass }> { text }</button>
+       );
+    }
+});
+
 ReactDOM.render(
     < Nav title="Learn React!" linkUrl="index.html"/>,
      document.getElementById('nav') 
@@ -34,4 +58,9 @@ ReactDOM.render(
 ReactDOM.render(
     < Title title="Learning React" />,
      document.getElementById('title') 
+);
+
+ReactDOM.render(
+    < Button text="My component button" textActive="Button Active"/>,
+     document.getElementById('button') 
 );
