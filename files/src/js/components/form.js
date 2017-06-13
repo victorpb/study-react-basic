b46f2de322b12fb22cd616970e34a62e263da197
Victor Pereira <victtor.vpb@gmail.com>
2017-06-12 23:27:18 -0300

    add contact

--- files/src/js/components/form.js
+++ files/src/js/components/form.js
@@ -1,5 +1,38 @@
 var Form = React.createClass({
 
+    getInitialState: function(){
+        return {name:'', email:'', subject:'R', messenger:''}
+    },
+
+    handleNameChange: function(e){
+        this.setState({ name: e.target.value })
+    },
+
+    handleEmailChange: function(e){
+        this.setState({ email: e.target.value })
+    },
+
+    handleSubjectChange: function(e){
+        this.setState({ subject: e.target.value })
+    },
+
+    handleMessengerChange: function(e){
+        this.setState({ messenger: e.target.value })
+    },
+
+    handleSubmit: function(e){
+        e.preventDefault();
+        var name = this.state.name.trim();
+        var email = this.state.email.trim();
+        var subject = this.state.subject;
+        var messenger = this.state.messenger.trim();
+        
+        if (!name|| !email || !subject|| !messenger ){
+            return confirm("error")
+        };
+        this.props.onContactSubmit({idNumber:this.props.idNumber.toString(), name:name, email:email, subject:subject, messenger:messenger})
+    },
+
     render: function(){
         var InputStyle = {
             padding: "20px",
@@ -7,18 +40,18 @@ var Form = React.createClass({
             color: "#A7A5A5"
         };
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" id="name" style={InputStyle} placeholder="name" />
+                    <input onChange={this.handleNameChange} type="text" className="form-control" id="name" style={InputStyle} placeholder="name" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" id="email" style={InputStyle} placeholder="email"/>
+                    <input onChange={this.handleEmailChange} type="email" className="form-control" id="email" style={InputStyle} placeholder="email"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="subject">Subject</label>
-                    <select defaultValue="R" className="form-control"  id="suject" >
+                    <select onChange={this.handleSubjectChange} defaultValue={this.state.subject} className="form-control"  id="suject" >
                         <option value="A">Angular</option>
                         <option value="J">Jquery</option>
                         <option value="R">React</option>
@@ -26,8 +59,9 @@ var Form = React.createClass({
                 </div>
                 <div className="form-group">
                     <label htmlFor="messenger">Email</label>
-                    <textarea row="3" className="form-control" id="messenger" style={InputStyle}> </textarea>
+                    <textarea onChange={this.handleMessengerChange}row="3" className="form-control" id="messenger" style={InputStyle}> </textarea>
                 </div>
+                <Button text="Submit" textActive="Wait..."/>
             </form>
         );
     },
--- files/src/js/main.js
+++ files/src/js/main.js
@@ -1,11 +1,21 @@
 
-var data = [
-    {idNumber:"1", name:"Maria", email:"maria@email.com", subject:"R", messenger:"Me messenger text"},
-    {idNumber:"2", name:"Joao", email:"joao@email.com", subject:"A", messenger:"Me messenger text 2"},
-]
-
 
 var Page = React.createClass({
+    getInitialState: function(){
+        return {data:[
+            {idNumber:"1", name:"Maria", email:"maria@email.com", subject:"R", messenger:"Me messenger text"},
+            {idNumber:"2", name:"Joao", email:"joao@email.com", subject:"A", messenger:"Me messenger text 2"},
+        ]}
+    },
+    handleContactSubmit: function(contact){
+        // console.log(contact)
+        // this.setState({data:this.state.data.push(contact)})
+        // console.log(this.state.data)
+        var newContacts = this.state.data;
+        newContacts.push(contact)
+        this.setState({data: newContacts});
+        
+    },
     render: function(){
         return(
             <myElement>
@@ -16,11 +26,10 @@ var Page = React.createClass({
                        <p>Learning React</p>
                     </Title>
                     <div className="row">
-                        <Form />
-                        <Button text="Submit" textActive="Wait..."/>
+                        <Form onContactSubmit={this.handleContactSubmit} idNumber={this.state.data.length + 1}/>
                     </div>
                     <div className="row">
-                        <List data={data} />
+                        <List data={this.state.data} />
                     </div>
                 </div>
             </myElement>
